c6af03404c590ff42d0ee23ccf1bd2258ea47eb0
Chris Wagner <jwag.wagner@gmail.com>
2019-04-28 13:26:18 -0700

    prepare for 3.02 (#33)

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ copyright = u'2012-2019, Matt Wright'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0.1'
+version = '3.0.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
