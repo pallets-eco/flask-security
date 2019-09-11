a7eac037c01a5d0be2870fcd98b5cfce5bb5eefe
Chris Wagner <jwag.wagner@gmail.com>
2019-09-11 16:19:17 -0700

    chore: Update/improve docs - new logo (#178)
    Continue converting to Pallet's sphinx style - this changed the sidebars - we no longer
    need our own versions.
    
    Change logo from helmut/armor to owl...
    
    Made content/first page more like flask.

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 
+from pallets_sphinx_themes import ProjectLink
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -49,6 +51,7 @@ master_doc = "index"
 # General information about the project.
 project = u"Flask-Security"
 copyright = u"2012-2019"
+author = "Matt Wright & Chris Wagner"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -104,21 +107,21 @@ html_theme = "flask"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+html_theme_options = {"index_sidebar_logo": False}
+html_context = {
+    "project_links": [
+        ProjectLink("PyPI releases", "https://pypi.org/project/Flask-Security-Too/"),
+        ProjectLink("Source Code", "https://github.com/jwag956/flask-security/"),
+        ProjectLink(
+            "Issue Tracker", "https://github.com/jwag956/flask-security/issues/"
+        ),
+    ]
+}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = None
+html_title = "Flask-Security Documentation ({}).format(version)"
+html_logo = "_static/logo-owl-105.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -130,55 +133,16 @@ html_theme_path = ["_themes"]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static", "openapi.yaml"]
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-# html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
-    "index": ["sidebarintro.html", "sourcelink.html", "searchbox.html"],
-    "**": [
-        "sidebarlogo.html",
-        "localtoc.html",
-        "relations.html",
-        "sourcelink.html",
-        "searchbox.html",
-    ],
+    "index": ["project.html", "localtoc.html", "searchbox.html"],
+    "**": ["localtoc.html", "relations.html", "searchbox.html"],
 }
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-# html_additional_pages = {}
-
-# If false, no module index is generated.
-# html_domain_indices = True
-
-# If false, no index is generated.
-# html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-# html_split_index = False
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
 
 # If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-# html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-# html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-# html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-# html_file_suffix = None
+html_show_sourcelink = False
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "Flask-Securitydoc"
@@ -199,13 +163,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto/manual]).
 latex_documents = [
-    (
-        "index",
-        "Flask-Security.tex",
-        u"Flask-Security Documentation",
-        u"Matt Wright",
-        "manual",
-    )
+    ("index", "Flask-Security.tex", u"Flask-Security Documentation", author, "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,18 +185,6 @@ latex_documents = [
 
 # If false, no module index is generated.
 # latex_domain_indices = True
-
-
-# -- Options for manual page output --------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ("index", "flask-security", u"Flask-Security Documentation", [u"Matt Wright"], 1)
-]
-
-# If true, show URL addresses after external links.
-# man_show_urls = False
 
 
 # -- Options for Texinfo output ------------------------------------------
