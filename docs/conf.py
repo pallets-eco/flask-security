0c6b731c2d74ffce9841ed718135159b94d88e33
jwag956 <jwag.wagner@gmail.com>
2021-01-07 21:31:16 -0800

    3.4.5 security patch

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ author = "Matt Wright & Chris Wagner"
 # built documents.
 #
 # The short X.Y version.
-version = "3.4.4"
+version = "3.4.5"
 # The full version, including alpha/beta/rc tags.
 release = version
 
